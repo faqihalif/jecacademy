ca4db78c65c2b11d492fcb6e442b842db514d70f
Faqih Alif <faqihalif@live.com>
2021-04-07 19:00:14 +0700

    .

--- src/pages/library/index.vue
+++ src/pages/library/index.vue
@@ -17,7 +17,7 @@
                     <div class="flex justify-end mb-4">
                         <span class="rounded-full px-2 font-bold text-sm bg-blue-500 text-white">{{ library.year }}</span>
                     </div>
-                    <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
+                    <div class="grid grid-cols-1 md:grid-cols-4 gap-8 mb-10">
                         <div v-for="(journal, index) in library.journals" :key="index">
                             <router-link :to="{name: 'LibraryDetail', params: {year: library.year, slug: journal.slug}}">
                                 <div class="flex flex-col">
